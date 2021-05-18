9857be2c3f7bdf4f2e0b8f24472a235f896fa9ec
whyrusleeping <why@ipfs.io>
2021-05-17 22:53:42 -0700

    add collections

---
+++ collections.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+type Collection struct {
+	ID        uint      `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+
+	UUID string `gorm:"index" json:"uuid"`
+
+	Name        string `gorm:"unique" json:"name"`
+	Description string `json:"description"`
+	UserID      uint   `json:"userId"`
+}
+
+type CollectionRef struct {
+	ID         uint `gorm:"primaryKey"`
+	CreatedAt  time.Time
+	Collection uint
+	Content    uint
+}
