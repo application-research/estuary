cd2294c8647370353df58e8e796f1493eb5ddcf8
whyrusleeping <why@ipfs.io>
2021-08-23 10:21:55 -0700

    add missing file

---
+++ util/unixfs.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"io"
+
+	"github.com/ipfs/go-cidutil"
+	chunker "github.com/ipfs/go-ipfs-chunker"
+	ipld "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-unixfs/importer/balanced"
+	ihelper "github.com/ipfs/go-unixfs/importer/helpers"
+	mh "github.com/multiformats/go-multihash"
+)
+
+var DefaultHashFunction = uint64(mh.SHA2_256)
+
+func ImportFile(dserv ipld.DAGService, fi io.Reader) (ipld.Node, error) {
+	prefix, err := merkledag.PrefixForCidVersion(1)
+	if err != nil {
+		return nil, err
+	}
+	prefix.MhType = DefaultHashFunction
+
+	spl := chunker.NewSizeSplitter(fi, 1024*1024)
+	dbp := ihelper.DagBuilderParams{
+		Maxlinks:  1024,
+		RawLeaves: true,
+
+		CidBuilder: cidutil.InlineBuilder{
+			Builder: prefix,
+			Limit:   126,
+		},
+
+		Dagserv: dserv,
+	}
+
+	db, err := dbp.New(spl)
+	if err != nil {
+		return nil, err
+	}
+
+	return balanced.Layout(db)
+}
