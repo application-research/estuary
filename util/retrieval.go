a21b7493a1bb7c7d8e30fc7663ab9b416a7ce1c6
whyrusleeping <why@ipfs.io>
2021-09-09 11:57:32 -0700

    add missing retrieval file

---
+++ util/retrieval.go
@@ -0,0 +1,12 @@
+package util
+
+import "gorm.io/gorm"
+
+type RetrievalFailureRecord struct {
+	gorm.Model
+	Miner   string `json:"miner"`
+	Phase   string `json:"phase"`
+	Message string `json:"message"`
+	Content uint   `json:"content"`
+	Cid     DbCID  `json:"cid"`
+}
