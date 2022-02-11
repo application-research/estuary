b6a267dada41794515f0d09fa8255234a63e55c4
Dirk McCormick <dirkmdev@gmail.com>
2022-02-11 14:38:27 +0100

    feat: util for calculating CAR size

---
+++ util/car.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"encoding/binary"
+
+	"github.com/ipfs/go-cid"
+	"github.com/ipld/go-car"
+)
+
+type Object struct {
+	Cid  cid.Cid
+	Size uint64
+}
+
+func CalculateCarSize(data cid.Cid, objects []Object) (uint64, error) {
+	// Calculate header size
+	header := car.CarHeader{
+		Roots:   []cid.Cid{data},
+		Version: 1,
+	}
+	size, err := car.HeaderSize(&header)
+	if err != nil {
+		return 0, err
+	}
+
+	// Calculate size of each block + metadata
+	buf := make([]byte, 8)
+	for _, o := range objects {
+		bz := o.Cid.Bytes()
+		sum := uint64(len(bz))           // size of cid
+		sum += o.Size                    // size of block data
+		n := binary.PutUvarint(buf, sum) // size of var int of cid + block size
+		size += sum + uint64(n)
+	}
+
+	return size, nil
+}
---
+++ util/car_test.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"context"
+	"io"
+	"math/rand"
+	"testing"
+
+	"github.com/filecoin-project/go-fil-markets/shared"
+	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-datastore"
+	dss "github.com/ipfs/go-datastore/sync"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipld/go-car"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCalculateCarSize(t *testing.T) {
+	ctx := context.Background()
+
+	ds := dss.MutexWrap(datastore.NewMapDatastore())
+	bs := blockstore.NewBlockstore(ds)
+	bserv := blockservice.New(bs, nil)
+	dserv := merkledag.NewDAGService(bserv)
+
+	rseed := 5
+	rawSize := 2 * 1024 * 1024
+	source := io.LimitReader(rand.New(rand.NewSource(int64(rseed))), int64(rawSize))
+	nd, err := ImportFile(dserv, source)
+	require.NoError(t, err)
+
+	payloadCid := nd.Cid()
+	selectiveCar := car.NewSelectiveCar(
+		ctx,
+		bs,
+		[]car.Dag{{Root: payloadCid, Selector: shared.AllSelector()}},
+		car.TraverseLinksOnlyOnce(),
+	)
+
+	objects := make([]Object, 0, 0)
+	preparedCar, err := selectiveCar.Prepare(func(block car.Block) error {
+		objects = append(objects, Object{
+			Cid:  block.BlockCID,
+			Size: uint64(len(block.Data)),
+		})
+		return nil
+	})
+	require.NoError(t, err)
+
+	err = preparedCar.Dump(ctx, io.Discard)
+	require.NoError(t, err)
+
+	size, err := CalculateCarSize(payloadCid, objects)
+	require.NoError(t, err)
+	require.Equal(t, int(preparedCar.Size()), int(size))
+}
