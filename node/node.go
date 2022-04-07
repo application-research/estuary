5170849fe964694eb7510930466491108181499f
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2022-04-07 15:02:06 -0300

    move index provider engine to node struct

--- node/node.go
+++ node/node.go
@@ -17,6 +17,7 @@ import (
 	"github.com/application-research/filclient/keystore"
 	autobatch "github.com/application-research/go-bs-autobatch"
 	lmdb "github.com/filecoin-project/go-bs-lmdb"
+	"github.com/filecoin-project/index-provider/engine"
 	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
@@ -98,11 +99,12 @@ type NodeInitializer interface {
 }
 
 type Node struct {
-	Dht      *dht.IpfsDHT
-	Provider *batched.BatchProvidingSystem
-	FullRT   *fullrt.FullRT
-	FilDht   *dht.IpfsDHT
-	Host     host.Host
+	Dht           *dht.IpfsDHT
+	Provider      *batched.BatchProvidingSystem
+	FullRT        *fullrt.FullRT
+	FilDht        *dht.IpfsDHT
+	Host          host.Host
+	IndexProvider *engine.Engine
 
 	// Set for gathering disk usage
 
