a8e7a835f621492c6b17abeb7d3d335cc9afaf8d
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2022-06-02 15:11:06 -0300

    improve bsget cli

--- cmd/bsget/main.go
+++ cmd/bsget/main.go
@@ -14,6 +14,7 @@ import (
 	ipld "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	rhelp "github.com/libp2p/go-libp2p-routing-helpers"
 	"github.com/multiformats/go-multiaddr"
@@ -23,16 +24,42 @@ import (
 )
 
 func main() {
-	app := cli.NewApp()
+	app := &cli.App{
+		Name:  "bsget",
+		Usage: "fetch CIDs from a peer's multiaddress",
+	}
+
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} {CID} {PEER_MULTIADDRESS} {{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}{{if .Copyright }}
+COPYRIGHT:
+   {{.Copyright}}
+   {{end}}{{if .Version}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
 
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
-			Name: "output",
+			Name:    "output",
+			Aliases: []string{"o"},
+			Usage:   "Specify file to which write the requested CIDs",
 		},
 	}
 	app.Action = func(cctx *cli.Context) error {
 		if cctx.Args().Len() < 2 {
-			return fmt.Errorf("must pass cid and multiaddr of peer to fetch from")
+			return fmt.Errorf("usage: ./bsget {CID} {PEER_MULTIADDRESS}")
 		}
 
 		root, err := cid.Decode(cctx.Args().Get(0))
@@ -51,7 +78,17 @@ func main() {
 
 		// set up libp2p node...
 		ctx := context.Background()
-		h, err := libp2p.New()
+		bytePk, err := crypto.ConfigDecodeKey("CAESQHHiijN4B16MLqKoQK/VgCBXy8fJ0PbLZWlr2x5HGGtxO/qPL96PDoD399cN2doCxG069r+wgs1rcOOPC8l+4Dk=")
+		if err != nil {
+			return err
+		}
+
+		pk, err := crypto.UnmarshalPrivateKey(bytePk)
+		if err != nil {
+			return err
+		}
+
+		h, err := libp2p.New(libp2p.Identity(pk))
 		if err != nil {
 			return err
 		}
@@ -85,12 +122,13 @@ func main() {
 			return node.Links(), nil
 
 		}
-		if err := merkledag.Walk(ctx, getLinks, root, cset.Visit, merkledag.Concurrency(500)); err != nil {
+		if err := merkledag.Walk(ctx, getLinks, root, cset.Visit, merkledag.Concurrency(2)); err != nil {
 			return err
 		}
 
 		bar.Finish()
 
+		fmt.Println("CIDs retrieved successfully")
 		return nil
 	}
 
