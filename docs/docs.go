c25e8076814796739e7f8f0453c031d21b5676c8
snissn <snissn@users.noreply.github.com>
2022-09-29 08:38:55 +0000

    docs: update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -26,7 +26,7 @@ const docTemplate = `{
     "paths": {
         "/admin/autoretrieve/init": {
             "post": {
-                "description": "This endpoint registers a new autoretrieve server Q@\u0026^5#^\u0026*Q^Wasdf",
+                "description": "This endpoint registers a new autoretrieve server Q@\u0026^5#^\u0026*Q^Wasdfalsdkjf",
                 "produces": [
                     "application/json"
                 ],
