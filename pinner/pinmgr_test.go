7bdb0829dde2afe9a1e4c4af3c38340a83629f61
mike seiler <snissn@gmail.com>
2022-10-13 16:40:16 -1000

    add lots of tests for loading data

--- pinner/pinmgr_test.go
+++ pinner/pinmgr_test.go
@@ -2,18 +2,22 @@ package pinner
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"testing"
 
 	"github.com/application-research/estuary/pinner/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func doPinning(ctx context.Context, op *PinningOperation, cb PinProgressCB) error {
+	fmt.Println("doPinning")
 	return nil
 }
 
 func onPinStatusUpdate(cont uint, location string, status types.PinningStatus) error {
+	fmt.Println("onPinStatusUpdate")
 	return nil
 }
 func newManager() *PinManager {
@@ -22,25 +26,74 @@ func newManager() *PinManager {
 	})
 }
 
-func newPinData(name string) PinningOperation {
+func newPinData(name string, userid int) PinningOperation {
 	return PinningOperation{
 		Name:   name,
-		UserId: 0,
+		UserId: uint(userid),
 	}
 }
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestHelloName(t *testing.T) {
+var N int = 10
+var sleeptime time.Duration = 100
+
+func TestSend1Pin(t *testing.T) {
+	i := 1
 	mgr := newManager()
 	go mgr.Run(0)
-	mike := newPinData("mike")
-	go mgr.Add(&mike)
-	go mgr.Add(&mike)
-	go mgr.Add(&mike)
-	go mgr.Add(&mike)
-	go mgr.Add(&mike)
-	time.Sleep(1 * time.Second)
-	t.Log("queue size", mgr.PinQueueSize())
+	pin := newPinData("name"+fmt.Sprint(i), i)
+	go mgr.Add(&pin)
 
+	time.Sleep(sleeptime * time.Millisecond)
+	assert.Equal(t, 0, mgr.PinQueueSize(), "first pin doesn't enter queue")
+}
+
+func TestNUniqueNames(t *testing.T) {
+	mgr := newManager()
+	go mgr.Run(0)
+	for i := 0; i < N; i++ {
+		pin := newPinData("name"+fmt.Sprint(i), i)
+		go mgr.Add(&pin)
+	}
+	time.Sleep(sleeptime * time.Millisecond)
+	assert.Equal(t, N-1, mgr.PinQueueSize(), "queue should have N pins in it")
+}
+
+func TestNUniqueNamesSameUser(t *testing.T) {
+	mgr := newManager()
+	go mgr.Run(0)
+	for j := 0; j < N; j++ {
+
+		for i := 0; i < N; i++ {
+			pin := newPinData("name"+fmt.Sprint(i), i)
+			go mgr.Add(&pin)
+		}
+	}
+	time.Sleep(sleeptime * time.Millisecond)
+	assert.Equal(t, N*N-1, mgr.PinQueueSize(), "queue should have N pins in it")
+}
+
+func TestNDuplicateNames(t *testing.T) {
+	mgr := newManager()
+	go mgr.Run(0)
+	for i := 0; i < N; i++ {
+		pin := newPinData("name", 0)
+		go mgr.Add(&pin)
+	}
+	time.Sleep(sleeptime * time.Millisecond)
+	assert.Equal(t, N-1, mgr.PinQueueSize(), "queue should have N pins in it")
+}
+
+func TestNDuplicateNamesNDuplicateUsersNTimes(t *testing.T) {
+	mgr := newManager()
+	go mgr.Run(0)
+	for k := 0; k < N; k++ {
+		for j := 0; j < N; j++ {
+			for i := 0; i < N; i++ {
+				pin := newPinData("name"+fmt.Sprint(i), j)
+				go mgr.Add(&pin)
+			}
+		}
+	}
+	time.Sleep(sleeptime * time.Millisecond)
+	assert.Equal(t, N*N*N-1, mgr.PinQueueSize(), "queue should have N pins in it")
 }
