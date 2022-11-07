0e5adaf3a40fda0a4eb1efd64a0e64c4a9b27844
Neel Virdy <neelvirdy@gmail.com>
2022-11-07 16:34:28 -0500

    Consolidate with similar check

--- replication.go
+++ replication.go
@@ -1506,12 +1506,8 @@ func (cm *ContentManager) ensureStorage(ctx context.Context, content util.Conten
 			return errors.Wrap(err, "could not retrieve dataCap from client balance")
 		}
 
-		if bl == nil {
+		if bl == nil || bl.VerifiedClientBalance == nil {
 			return errors.New("verifed deals requires datacap, please see https://verify.glif.io or use the --verified-deal=false for non-verified deals")
-		}
-
-		if bl.VerifiedClientBalance == nil {
-			return errors.Wrap(err, "client balance has nil dataCap")
 		}
 
 		if bl.VerifiedClientBalance.LessThan(big.NewIntUnsigned(uint64(abi.UnpaddedPieceSize(content.Size).Padded()))) {
