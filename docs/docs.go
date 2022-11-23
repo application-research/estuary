2785971bc8b83baa175ff0361fba9310b0cd845d
anjor <anjor@users.noreply.github.com>
2022-11-23 14:19:12 +0000

    docs: update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -716,6 +716,12 @@ const docTemplate = `{
                                 "type": "integer"
                             }
                         }
+                    },
+                    {
+                        "type": "string",
+                        "description": "Directory",
+                        "name": "dir",
+                        "in": "query"
                     }
                 ],
                 "responses": {
@@ -897,7 +903,7 @@ const docTemplate = `{
                     },
                     {
                         "type": "string",
-                        "description": "Filenam to use for upload",
+                        "description": "Filename to use for upload",
                         "name": "filename",
                         "in": "formData"
                     },
