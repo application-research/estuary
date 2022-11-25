566bf34b4c2600934e457ab4e9d4e0fefc9d84cf
Lawrence Enehizena <lawstands@gmail.com>
2022-11-25 17:44:44 +0100

    implement the actual content marker

---
+++ contentmgr/sanity_check.go
@@ -0,0 +1,25 @@
+package contentmgr
+
+import (
+	"github.com/application-research/estuary/util"
+	"github.com/ipfs/go-cid"
+)
+
+func (cm *ContentManager) HandleSanityCheck(cc cid.Cid, err error) {
+	// get a contents affected by this missing block
+	// Todo - use group for query
+	var cnts []util.Content
+	where := "id in (select content from obj_refs where object = (select id from objects where cid = ?))"
+	if err := cm.DB.Find(&cnts, where, cc.Bytes()).Error; err != nil {
+		cm.log.Errorf("sanity check failed to get content(s) for cid: %s, err: %w", cc.String(), err)
+		return
+	}
+
+	// mark all contents affected by this missing block
+	for _, cnt := range cnts {
+		if err := cm.DB.Model(&util.Content{}).Where("id = ?", cnt.ID).Update("failed_sanity_check", true).Error; err != nil {
+			cm.log.Errorf("sanity check failed to mark content: %d, err: %w", cnt.ID, err)
+			return
+		}
+	}
+}
