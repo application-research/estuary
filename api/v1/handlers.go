d3228bc5acdb4f70c45a6c35d49dbfd2167122b8
anjor <anjor@umd.edu>
2022-12-08 14:20:38 +0000

    tests

--- api/v1/handlers.go
+++ api/v1/handlers.go
@@ -3427,7 +3427,7 @@ func (s *apiV1) handleCommitCollection(c echo.Context, u *util.User) error {
 // @Description  This endpoint is used to get contents in a collection. If no colpath query param is passed
 // @Tags         collections
 // @Produce      json
-// @Success      200  {object}  []collectionListResponse
+// @Success      200  {object}  []collections.CollectionListResponse
 // @Failure      400  {object}  util.HttpError
 // @Failure      500  {object}  util.HttpError
 // @Param        coluuid  path      string  true   "coluuid"
@@ -3470,79 +3470,12 @@ func (s *apiV1) handleGetCollectionContents(c echo.Context, u *util.User) error 
 	// if queryDir is set, do the content listing
 	queryDir = filepath.Clean(queryDir)
 
-	dirs := make(map[string]bool)
-	var out []collectionListResponse
-	for _, r := range refs {
-		if r.Path == "" || r.Name == "" {
-			continue
-		}
-
-		if !strings.HasPrefix(r.Path, queryDir) {
-			continue
-		}
-
-		relp, err := getRelativePath(r, queryDir)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, fmt.Errorf("errored while calculating relative contentPath queryDir=%s, contentPath=%s", queryDir, r.Path))
-		}
-
-		if relp == "." { // Query directory is the complete path containing the content.
-			// trying to list a CID queryDir, not allowed
-			if r.Type == util.Directory {
-				return c.JSON(http.StatusBadRequest, fmt.Errorf("listing CID directories is not allowed"))
-			}
-
-			out = append(out, collectionListResponse{
-				Name:      r.Name,
-				Type:      File,
-				Size:      r.Size,
-				ContID:    r.ID,
-				Cid:       &util.DbCID{CID: r.Cid.CID},
-				Dir:       queryDir,
-				ColUuid:   coluuid,
-				UpdatedAt: r.UpdatedAt,
-			})
-		} else {
-
-			// Query directory has a subdirectory, which contains the actual content.
-			// if relative contentPath has a /, the file is in a subdirectory
-			// print the directory the file is in if we haven't already
-			if strings.Contains(relp, "/") {
-				parts := strings.Split(relp, "/")
-				subDir := parts[0]
-				if !dirs[subDir] {
-					dirs[subDir] = true
-					out = append(out, collectionListResponse{
-						Name:      subDir,
-						Type:      Dir,
-						Dir:       queryDir,
-						ColUuid:   coluuid,
-						UpdatedAt: r.UpdatedAt,
-					})
-					continue
-				}
-			} else {
-				out = append(out, collectionListResponse{
-					Name:      r.Name,
-					Type:      File,
-					Size:      r.Size,
-					ContID:    r.ID,
-					Cid:       &util.DbCID{CID: r.Cid.CID},
-					Dir:       queryDir,
-					ColUuid:   coluuid,
-					UpdatedAt: r.UpdatedAt,
-				})
-
-			}
-		}
+	out, err := collections.GetDirectoryContents(c, refs, queryDir, coluuid)
+	if err != nil {
+		return err
 	}
-	return c.JSON(http.StatusOK, out)
-}
 
-func getRelativePath(r util.ContentWithPath, queryDir string) (string, error) {
-	contentPath := r.Path
-	relp, err := filepath.Rel(queryDir, contentPath)
-	return relp, err
+	return c.JSON(http.StatusOK, out)
 }
 
 // handleDeleteCollection godoc
@@ -5126,21 +5059,8 @@ func (s *apiV1) handleShuttleRepinAll(c echo.Context) error {
 type CidType string
 
 const (
-	Dir    CidType = "directory"
-	File   CidType = "file"
-	ColDir string  = "dir"
+	ColDir string = "dir"
 )
-
-type collectionListResponse struct {
-	Name      string      `json:"name"`
-	Type      CidType     `json:"type"`
-	Size      int64       `json:"size"`
-	ContID    uint        `json:"contId"`
-	Cid       *util.DbCID `json:"cid,omitempty"`
-	Dir       string      `json:"dir"`
-	ColUuid   string      `json:"coluuid"`
-	UpdatedAt time.Time   `json:"updatedAt"`
-}
 
 func sanitizePath(p string) (string, error) {
 	if len(p) == 0 {
--- collections/collections.go
+++ collections/collections.go
@@ -1,8 +1,11 @@
 package collections
 
 import (
+	"errors"
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"net/http"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -30,6 +33,29 @@ type CollectionRef struct {
 	Content    uint    `gorm:"index:,option:CONCURRENTLY;not null"`
 	Path       *string `gorm:"not null"`
 }
+
+type CidType string
+
+type CollectionListResponse struct {
+	Name      string      `json:"name"`
+	Type      CidType     `json:"type"`
+	Size      int64       `json:"size"`
+	ContID    uint        `json:"contId"`
+	Cid       *util.DbCID `json:"cid,omitempty"`
+	Dir       string      `json:"dir"`
+	ColUuid   string      `json:"coluuid"`
+	UpdatedAt time.Time   `json:"updatedAt"`
+}
+
+const (
+	CidTypeDir  CidType = "directory"
+	CidTypeFile CidType = "file"
+)
+
+const (
+	ERR_INTERNAL_SERVER = "ERR_INTERNAL_SERVER"
+	ERR_BAD_REQUEST     = "ERR_BAD_REQUEST"
+)
 
 func GetCollection(coluuid string, db *gorm.DB, u *util.User) (Collection, error) {
 	var col Collection
@@ -74,4 +100,100 @@ func GetContentsInPath(coluuid string, path string, db *gorm.DB, u *util.User) (
 	}
 
 	return selectedRefs, nil
+}
+
+func GetDirectoryContents(c echo.Context, refs []util.ContentWithPath, queryDir, coluuid string) ([]CollectionListResponse, error) {
+	dirs := make(map[string]bool)
+	var result []CollectionListResponse
+	for _, r := range refs {
+		directoryContent, traversedDirectories, err := getDirectoryContent(r, dirs, queryDir, coluuid)
+		dirs = traversedDirectories
+
+		if err != nil {
+			if err == errors.New(ERR_INTERNAL_SERVER) {
+				return nil, c.JSON(http.StatusInternalServerError, fmt.Errorf("errored while calculating relative contentPath queryDir=%s, contentPath=%s", queryDir, r.Path))
+			}
+
+			if err == errors.New(ERR_BAD_REQUEST) {
+				return nil, c.JSON(http.StatusInternalServerError, fmt.Errorf("errored while calculating relative contentPath queryDir=%s, contentPath=%s", queryDir, r.Path))
+			}
+			return nil, err
+		}
+
+		if directoryContent == (CollectionListResponse{}) {
+			result = append(result, directoryContent)
+		}
+	}
+	return result, nil
+}
+
+func getDirectoryContent(r util.ContentWithPath, dirs map[string]bool, queryDir, coluuid string) (CollectionListResponse, map[string]bool, error) {
+	if r.Path == "" || r.Name == "" {
+		return CollectionListResponse{}, dirs, nil
+	}
+
+	if !strings.HasPrefix(r.Path, queryDir) {
+		return CollectionListResponse{}, dirs, nil
+	}
+
+	relp, err := getRelativePath(r, queryDir)
+	if err != nil {
+		//return collections.CollectionListResponse{}, dirs, c.JSON(http.StatusInternalServerError, fmt.Errorf("errored while calculating relative contentPath queryDir=%s, contentPath=%s", queryDir, r.Path))
+		return CollectionListResponse{}, dirs, errors.New(ERR_INTERNAL_SERVER)
+	}
+
+	// Query directory is the complete path containing the content.
+	if relp == "." {
+		// trying to list a CID queryDir, not allowed
+		if r.Type == util.Directory {
+			//return collections.CollectionListResponse{}, dirs, c.JSON(http.StatusBadRequest, fmt.Errorf("listing CID directories is not allowed"))
+			return CollectionListResponse{}, dirs, errors.New(ERR_BAD_REQUEST)
+		}
+
+		return CollectionListResponse{
+			Name:      r.Name,
+			Type:      CidTypeFile,
+			Size:      r.Size,
+			ContID:    r.ID,
+			Cid:       &util.DbCID{CID: r.Cid.CID},
+			Dir:       queryDir,
+			ColUuid:   coluuid,
+			UpdatedAt: r.UpdatedAt,
+		}, dirs, nil
+	}
+
+	// Query directory has a subdirectory, which contains the actual content.
+	// if relative contentPath has a /, the file is in a subdirectory
+	// print the directory the file is in if we haven't already
+	if strings.Contains(relp, "/") {
+		parts := strings.Split(relp, "/")
+		subDir := parts[0]
+		if !dirs[subDir] {
+			dirs[subDir] = true
+			return CollectionListResponse{
+				Name:      subDir,
+				Type:      CidTypeDir,
+				Dir:       queryDir,
+				ColUuid:   coluuid,
+				UpdatedAt: r.UpdatedAt,
+			}, dirs, nil
+		}
+	}
+
+	return CollectionListResponse{
+		Name:      r.Name,
+		Type:      CidTypeFile,
+		Size:      r.Size,
+		ContID:    r.ID,
+		Cid:       &util.DbCID{CID: r.Cid.CID},
+		Dir:       queryDir,
+		ColUuid:   coluuid,
+		UpdatedAt: r.UpdatedAt,
+	}, dirs, nil
+}
+
+func getRelativePath(r util.ContentWithPath, queryDir string) (string, error) {
+	contentPath := r.Path
+	relp, err := filepath.Rel(queryDir, contentPath)
+	return relp, err
 }
