7422cb6f05647e6804cd5a59838dcebc8f7afa5a
Jason Cihelka <jcihelka@isotechnics.com>
2022-12-08 10:57:01 -0800

    feat: add PingMany

--- cmd/estuary-shuttle/ping.go
+++ cmd/estuary-shuttle/ping.go
@@ -11,7 +11,30 @@ import (
 	"github.com/multiformats/go-multiaddr"
 )
 
-// Perform a libp2p network ping to a specified multiaddr/peer, returning the Round trip time (RTT)
+type Libp2pHost struct {
+	addr   multiaddr.Multiaddr
+	peerID peer.ID
+}
+
+type PingManyResult map[peer.ID]time.Duration
+
+// Ping a list of hosts, returning RTT for each of them. Errors will be ignored, unresponsive hosts will not be returned
+func (s *Shuttle) PingMany(ctx context.Context, hosts []Libp2pHost) PingManyResult {
+	result := make(PingManyResult)
+
+	// TODO: Batch them in go funcs for faster performance
+	for _, h := range hosts {
+		res, err := s.PingOne(ctx, h.addr, h.peerID)
+
+		if err == nil {
+			result[h.peerID] = *res
+		}
+	}
+
+	return result
+}
+
+// Perform a libp2p network ping to a specified host, returning the Round trip time (RTT)
 func (s *Shuttle) PingOne(ctx context.Context, addr multiaddr.Multiaddr, pID peer.ID) (*time.Duration, error) {
 	// Add the host to the peerstore so it can be found
 	s.Node.Host.Peerstore().AddAddr(pID, addr, peerstore.TempAddrTTL)
