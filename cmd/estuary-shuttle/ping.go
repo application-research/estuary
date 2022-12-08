5c3d34ea82a62c329f3cebb529593b07adf7e94b
Jason Cihelka <jcihelka@isotechnics.com>
2022-12-08 13:55:49 -0800

    feat: add PeerPingManager

--- cmd/estuary-shuttle/ping.go
+++ cmd/estuary-shuttle/ping.go
@@ -12,6 +12,11 @@ import (
 	"github.com/multiformats/go-multiaddr"
 )
 
+type PeerPingManager struct {
+	*Shuttle
+	Result PingManyResult
+}
+
 type Libp2pHost struct {
 	addr   multiaddr.Multiaddr
 	peerID peer.ID
@@ -20,30 +25,30 @@ type Libp2pHost struct {
 type PingManyResult map[peer.ID]time.Duration
 
 // Ping a list of hosts, returning RTT for each of them. Errors will be ignored, unresponsive hosts will not be returned
-func (s *Shuttle) PingMany(ctx context.Context, hosts []Libp2pHost) PingManyResult {
+func (ppm PeerPingManager) PingMany(ctx context.Context, hosts []Libp2pHost) {
 	result := make(PingManyResult)
 
 	// TODO: Batch them in go funcs for faster performance
 	for _, h := range hosts {
-		res, err := s.PingOne(ctx, h.addr, h.peerID)
+		res, err := ppm.pingOne(ctx, h.addr, h.peerID)
 
 		if err == nil {
 			result[h.peerID] = *res
 		}
 	}
 
-	return result
+	ppm.Result = result
 }
 
 // Perform a libp2p network ping to a specified host, returning the Round trip time (RTT)
-func (s *Shuttle) PingOne(ctx context.Context, addr multiaddr.Multiaddr, pID peer.ID) (*time.Duration, error) {
+func (ppm PeerPingManager) pingOne(ctx context.Context, addr multiaddr.Multiaddr, pID peer.ID) (*time.Duration, error) {
 	// Add the host to the peerstore so it can be found
-	s.Node.Host.Peerstore().AddAddr(pID, addr, peerstore.TempAddrTTL)
+	ppm.Node.Host.Peerstore().AddAddr(pID, addr, peerstore.TempAddrTTL)
 
 	tctx, cancel := context.WithTimeout(ctx, time.Duration(time.Millisecond*1000))
 	defer cancel()
 
-	t, err := netPing(tctx, s.Node.Host, pID)
+	t, err := netPing(tctx, ppm.Node.Host, pID)
 	if err != nil {
 		return nil, err
 	}
