da80e1bcd8ec5d93394a4bd52f1e49d962f332ac
Lawrence Enehizena <lawstands@gmail.com>
2022-12-16 13:33:01 +0100

    fix pinner test

--- pinner/pinmgr_test.go
+++ pinner/pinmgr_test.go
@@ -352,7 +352,7 @@ func TestNDuplicateNamesNDuplicateUsersNTimes(t *testing.T) {
 
 	for k := 0; k < N; k++ {
 		for j := 0; j < N; j++ {
-			for i := 0; i < N; i++ {
+			for i := 1; i <= N; i++ {
 				pin := newPinData("name"+fmt.Sprint(i), j, i)
 				go mgr.Add(&pin)
 			}
