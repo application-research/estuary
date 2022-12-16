491c9460496066b3957c03f92af19636bb900741
Lawrence Enehizena <lawstands@gmail.com>
2022-12-16 05:45:31 +0100

    respect api queue engine

--- shuttle/rpc/rpc.go
+++ shuttle/rpc/rpc.go
@@ -110,16 +110,17 @@ func (m *manager) SendRPCMessage(ctx context.Context, handle string, cmd *rpceve
 		return fmt.Errorf("attempted to send command to empty shuttle handle or local")
 	}
 
+	// if estuary has queue enabled, use it
+	if m.cfg.RpcEngine.Queue.Enabled && m.queueEng != nil {
+		if !rpcevent.CommandTopics[cmd.Op] {
+			return fmt.Errorf("%s topic has not been registered properly", cmd.Op)
+		}
+		m.log.Debugf("sending rpc message: %s, to shuttle: %s using queue engine", cmd.Op, handle)
+		return m.queueEng.SendMessage(cmd.Op, handle, cmd)
+	}
+
 	d, ok := m.websocketEng.GetShuttleConnection(handle)
 	if ok {
-		// if shutlle has rpc queue engine enabled and estuary has rpc queue enabled, use it
-		if d.QueueEngEnabled && m.queueEng != nil {
-			if !rpcevent.CommandTopics[cmd.Op] {
-				return fmt.Errorf("%s topic has not been registered properly", cmd.Op)
-			}
-			m.log.Debugf("sending rpc message: %s, to shuttle: %s using queue engine", cmd.Op, handle)
-			return m.queueEng.SendMessage(cmd.Op, handle, cmd)
-		}
 		m.log.Debugf("sending rpc message: %s, to shuttle: %s using websocket engine", cmd.Op, handle)
 		return d.SendMessage(ctx, cmd)
 	}
