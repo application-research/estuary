b411db77b81c0119947bb127d1469bf1c73c2be4
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2023-01-26 15:11:43 -0500

    fix duplicate index name

--- model/splitqueue.go
+++ model/splitqueue.go
@@ -9,8 +9,8 @@ import (
 type SplitQueue struct {
 	gorm.Model
 	ID            uint64    `gorm:"primarykey" json:"-"`
-	UserID        uint64    `gorm:"index;not null" json:"-"`
-	ContID        uint64    `gorm:"index;not null" json:"-"`
+	UserID        uint64    `gorm:"index;index:splits_user_id_cont_id;not null" json:"-"`
+	ContID        uint64    `gorm:"index;unique;index:splits_user_id_cont_id;not null" json:"-"`
 	Failing       bool      `gorm:"not null" json:"-"`
 	Attempted     uint      `gorm:"index:attempted_next_attempt_at;index;not null" json:"-"`
 	NextAttemptAt time.Time `gorm:"index:attempted_next_attempt_at;index;not null" json:"-"`
