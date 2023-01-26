5f1f2e885f5899bc6b9878b01e83bd89bf42bf52
Lawrence Enehizena <lawstands@gmail.com>
2023-01-26 23:39:13 +0100

    fix deal and split queue composite index

--- model/splitqueue.go
+++ model/splitqueue.go
@@ -9,8 +9,8 @@ import (
 type SplitQueue struct {
 	gorm.Model
 	ID            uint64    `gorm:"primarykey" json:"-"`
-	UserID        uint64    `gorm:"index;index:splits_user_id_cont_id;not null" json:"-"`
-	ContID        uint64    `gorm:"index;unique;index:splits_user_id_cont_id;not null" json:"-"`
+	UserID        uint64    `gorm:"index:splits_user_id_cont_id;not null;index" json:"-"`
+	ContID        uint64    `gorm:"index:splits_user_id_cont_id;not null;index;unique" json:"-"`
 	Failing       bool      `gorm:"not null" json:"-"`
 	Attempted     uint      `gorm:"index:attempted_next_attempt_at;index;not null" json:"-"`
 	NextAttemptAt time.Time `gorm:"index:attempted_next_attempt_at;index;not null" json:"-"`
