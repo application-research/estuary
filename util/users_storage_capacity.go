54d8473c67ae46fe6f78f03b4e108c1a5f983e6e
luc <luc.roy@gmail.com>
2023-05-09 10:56:07 -0300

    added default for soft / hard limit

--- util/users_storage_capacity.go
+++ util/users_storage_capacity.go
@@ -9,15 +9,15 @@ type UsersStorageCapacity struct {
 
 	UserId    uint
 	Size      int64
-	SoftLimit int64
-	HardLimit int64
+	SoftLimit int64 `gorm:"default:1319413953331"`
+	HardLimit int64 `gorm:"default:1649267441664"`
 }
 
 func (usc *UsersStorageCapacity) GetUserStorageCapacity(user *User, db *gorm.DB) error {
-	if err := db.Find(&usc, "user_id = ?", user.ID).Error; err != nil {
+	if err := db.First(&usc, "user_id = ?", user.ID).Error; err != nil {
 		usc.Size = 0
 		usc.UserId = user.ID
-		db.Save(&usc)
+		db.Create(&usc)
 	}
 	return nil
 }
