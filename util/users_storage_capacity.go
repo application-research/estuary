71e1f3ace3fa28576f754f7acc619b130b9ee233
luc <luc.roy@gmail.com>
2023-05-23 09:50:51 -0300

    change threshold field to have _bytes at the end for clarification

--- util/users_storage_capacity.go
+++ util/users_storage_capacity.go
@@ -10,8 +10,8 @@ type UsersStorageCapacity struct {
 
 	UserId     uint      `json:"user_id"`
 	Size       int64     `json:"size" gorm:"default:0"`
-	SoftLimit  int64     `json:"soft_limit" gorm:"default:1319413953331"` // Hardlimit*.8
-	HardLimit  int64     `json:"hard_limit" gorm:"default:1649267441664"` // 1.5TB
+	SoftLimit  int64     `json:"soft_limit_bytes" gorm:"default:1319413953331"` // Hardlimit*.8
+	HardLimit  int64     `json:"hard_limit_bytes" gorm:"default:1649267441664"` // 1.5TB
 	LastSyncAt time.Time `json:"last_sync_at"`
 }
 
